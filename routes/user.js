228847d8aa3d71542a33a157b674bc92074b1bee
BelleZarocha <91052520+SAROCHA042@users.noreply.github.com>
2023-01-30 08:15:43 +0700

    edit

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,15 @@
+// const express = require("express");
+// const router = express.Router();
+// const ctrl = require("../controllers");
+
+// router.get("/", ctrl.user.test);
+// router.post("/signup", ctrl.user.signup);
+// router.post("/login", ctrl.user.login);
+// router.put("/edit", ctrl.user.edit);
+// router.get("/editprofile", ctrl.user.getUser);
+
+// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
 const ctrl = require("../controllers");
@@ -6,6 +18,6 @@ router.get("/", ctrl.user.test);
 router.post("/signup", ctrl.user.signup);
 router.post("/login", ctrl.user.login);
 router.put("/edit", ctrl.user.edit);
-router.get("/editprofile", ctrl.user.getUser);
+router.get("/getprofile", ctrl.user.getUser);
 
 module.exports = router;
